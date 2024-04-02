15abdd8124ae57755fb159f39103cb3ce8b2161d
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-04-02 21:29:04 +0200

    Updating theme

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 
+import { ThemeContext } from "../../hooks/ThemeContext";
 
 import "../../styles/header.css";
 
 const Header = () => {
+  const { colors, spacing, typography } = useContext(ThemeContext);
   return (
-    <header className="header">
+    <header
+      className="header"
+      style={{ backgroundColor: colors.secondary.main }}
+    >
       <h1>Header</h1>
     </header>
   );
--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -19,6 +19,26 @@ const Theme = {
   },
   typography: {
     fontFamily: "Roboto, sans-serif",
+    h1: {
+      fontSize: "2rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "1.5rem",
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: "1.17rem",
+      fontWeight: 700,
+    },
+    h4: {
+      fontSize: "1rem",
+      fontWeight: 700,
+    },
+    h5: {
+      fontSize: ".83rem",
+      fontWeight: 700,
+    },
   },
   spacing: {
     padding: {
