84457881271e8b214b5880c0d1427258444fbcd8
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-04-02 21:50:09 +0200

    Addeng header elements

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -11,7 +11,51 @@ const Header = () => {
       className="header"
       style={{ backgroundColor: colors.secondary.main }}
     >
-      <h1>Header</h1>
+      <div className="header_container">
+        <div className="header_logo">
+          <h1
+            style={{
+              color: colors.primary.main,
+              ...typography.h1,
+            }}
+          >
+            Logo
+          </h1>
+        </div>
+        <nav className="header_nav">
+          <ul>
+            <li>
+              <a href="#" style={{ color: colors.primary.main }}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" style={{ color: colors.primary.main }}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" style={{ color: colors.primary.main }}>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" style={{ color: colors.primary.main }}>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <button
+            style={{
+              backgroundColor: colors.accent.main,
+              color: colors.secondary.main,
+              ...spacing.padding,
+            }}
+          >
+            Learn more
+          </button>
+        </nav>
+      </div>
     </header>
   );
 };
