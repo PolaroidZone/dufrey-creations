60b170c6efeb280c7736df43e826d2232f3316e5
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-04-03 13:09:12 +0200

    homepage strings

---
+++ src/components/sections/FeatureSection.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function FeatureSection() {
+  return (
+    <section>
+      <div className="container">
+        <div className="box">
+          <div className="content__container">
+            <div className="container_top"></div>
+            <div className="container_bottom">
+              <ul>
+                <li>
+                  <img />
+                  <h3>Feature 1</h3>
+                  <p></p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="image__container"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
