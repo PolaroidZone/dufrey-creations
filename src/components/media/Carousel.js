51bf0f88b2b8535e9ed6743825f2050a323f2675
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-04-03 10:29:14 +0200

    populating images

--- src/components/media/Carousel.js
+++ src/components/media/Carousel.js
@@ -1,49 +1,28 @@
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 
 import "../../styles/display/carousel.css";
 
+import { images } from "../../constants/images";
+
 export default function Carousel() {
+  console.log(images);
   return (
     <div className="carousel">
       <div className="top_carousel">
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
+        {images.carousel1.map((image) => (
+          <div key={image.id} className="item_carousel">
+            <img src={image.src} alt={image.alt} />
+          </div>
+        ))}
       </div>
       <div className="bottom_carousel">
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
-        <div className="item_carousel">
-          <img src="https://via.placeholder.com/150" alt="carausel" />
-        </div>
+        {images.carousel2.map((image) => (
+          <div key={image.id} className="item_carousel">
+            <img src={image.src} alt={image.alt} />
+          </div>
+        ))}
       </div>
     </div>
   );
