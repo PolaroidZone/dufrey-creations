1b66cf267a98a2b0cbba652a3e68f25f7f829062
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-04-05 19:01:35 +0200

    Create StatsSection.js

---
+++ src/components/sections/StatsSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function StatsSection() {
+  return (
+    <section className="stats_section">
+      <div className="stats_container">
+        <div className="stats_box"></div>
+
+        <div className="stats_box"></div>
+      </div>
+    </section>
+  );
+}
