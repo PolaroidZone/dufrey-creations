691da4dd5f8c6b30a9c7b04ad90a659ecc7e6739
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-04-06 13:52:41 +0200

    added themeing

--- src/components/sections/CTASection.js
+++ src/components/sections/CTASection.js
@@ -3,9 +3,11 @@ import React, { useContext } from "react";
 import { ThemeContext } from "../../hooks/ThemeContext";
 
 import siteStrings from "../../assets/data/siteStrings.json";
+import DefaultButton from "../buttons/DefaultButton";
 
 export default function CTASection() {
   const { colors, typohraphy } = useContext(ThemeContext);
+  const { heading2, paragraph } = typohraphy;
 
   const ctaStrings = siteStrings.ctaSection;
 
@@ -13,10 +15,26 @@ export default function CTASection() {
     <section className="cta_section">
       <div className="cta_container">
         <div className="cta_content">
-        <h2>{ctaStrings.title}</h2>
-        <p>{ctaStrings.description}</p>
+          <h2
+            style={{
+              fontSize: heading2.fontSize,
+              fontWeight: heading2.fontWeight,
+            }}
+          >
+            {ctaStrings.title}
+          </h2>
+          <p
+            style={{
+              fontSize: paragraph.fontSize,
+              fontweight: paragraph.fontWeight,
+            }}
+          >
+            {ctaStrings.description}
+          </p>
         </div>
-        <div className="cta_buttons"></div>
+        <div className="cta_buttons">
+          <DefaultButton text={"Learn More"} />
+        </div>
       </div>
     </section>
   );
