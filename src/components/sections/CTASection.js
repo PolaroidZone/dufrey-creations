e3862c16d3f7cc728909b6cd9c6f4a46e3463589
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-04-06 15:45:44 +0200

    Styled cta

--- src/components/sections/CTASection.js
+++ src/components/sections/CTASection.js
@@ -4,10 +4,13 @@ import { ThemeContext } from "../../hooks/ThemeContext";
 
 import siteStrings from "../../assets/data/siteStrings.json";
 import DefaultButton from "../buttons/DefaultButton";
+import TransparentButton from "../buttons/TransparentButton";
+
+import "../../styles/ctastyles.css";
 
 export default function CTASection() {
-  const { colors, typohraphy } = useContext(ThemeContext);
-  const { heading2, paragraph } = typohraphy;
+  const { colors, typography } = useContext(ThemeContext);
+  const { heading2, paragraph } = typography;
 
   const ctaStrings = siteStrings.ctaSection;
 
@@ -19,6 +22,9 @@ export default function CTASection() {
             style={{
               fontSize: heading2.fontSize,
               fontWeight: heading2.fontWeight,
+              color: colors.primary.main,
+              marginBottom: ".5em",
+              maxWidth: 700,
             }}
           >
             {ctaStrings.title}
@@ -27,13 +33,17 @@ export default function CTASection() {
             style={{
               fontSize: paragraph.fontSize,
               fontweight: paragraph.fontWeight,
+              color: colors.primary.main,
+              marginBottom: "2em",
+              maxWidth: 500,
             }}
           >
-            {ctaStrings.description}
+            {ctaStrings.details}
           </p>
         </div>
         <div className="cta_buttons">
           <DefaultButton text={"Learn More"} />
+          <TransparentButton text={"Contact"} isBorder={true} />
         </div>
       </div>
     </section>
